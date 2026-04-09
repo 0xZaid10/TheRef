f46192f0bf28d97b0ffac14ab56d82da37dcfaef
Zaid <soltrader101@gmail.com>
2026-04-09 16:57:05 +0530

    Update networks.ts

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -34,7 +34,7 @@ export const NETWORKS: Record<NetworkId, NetworkConfig> = {
     walletEnabled: false,
     speed:       "Fast",
     addresses: {
-      CORE:    process.env.NEXT_PUBLIC_STUDIONET_CORE    ?? "0x58B9f8b7D7B448C6D56C22A90556c632CB2F78FA",
+      CORE:    process.env.NEXT_PUBLIC_STUDIONET_CORE    ?? "0x7AC0872D81C502dEbC18F6799Bb465E9E452AA78",
       CORE_V1: process.env.NEXT_PUBLIC_STUDIONET_CORE_V1 ?? "0x88CAA18419714aA38CdF53c0E603141c48fa3238",
       LB:      process.env.NEXT_PUBLIC_STUDIONET_LB      ?? "0x8A2d05Df048A64cc6B83682a431ade05030e4BBB",
       ORG:     process.env.NEXT_PUBLIC_STUDIONET_ORG     ?? "0x265ef96A5230F13836c553D7DD2B9D7c3fE14aE1",
@@ -53,7 +53,7 @@ export const NETWORKS: Record<NetworkId, NetworkConfig> = {
     walletEnabled: true,
     speed:       "Public Testnet",
     addresses: {
-      CORE:    process.env.NEXT_PUBLIC_BRADBURY_CORE    ?? "0x05add101F032307cB05fd58Ba1EC37F545280A55",
+      CORE:    process.env.NEXT_PUBLIC_BRADBURY_CORE    ?? "0x96D9Bb0E5f47016d49c11C439263B84379c19623",
       CORE_V1: process.env.NEXT_PUBLIC_BRADBURY_CORE_V1 ?? "0xA29CfFC83d32fe924cFf1F1bDCf21555CCC96206",
       LB:      process.env.NEXT_PUBLIC_BRADBURY_LB      ?? "0x5D417F296b17656c9b950236feE66F63E22d8A54",
       ORG:     process.env.NEXT_PUBLIC_BRADBURY_ORG     ?? "0x440b28afc1804fc1E4AA8f5b559C18F7bCf43B3A",
